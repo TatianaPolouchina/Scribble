90e685df134793597f832cec75919a16c7075146
Tatiana <tpolouch@student.ubc.ca>
2024-06-06 16:05:55 -0400

    Recycler view now displays ongoing worries

--- app/src/main/java/com/example/worryapp/OngoingWorriesFragment.java
+++ app/src/main/java/com/example/worryapp/OngoingWorriesFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -26,15 +27,7 @@ public class OngoingWorriesFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_ongoing_worries, container, false);
-
-        //Set up recycler view
-        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
-        List<WorryItem> data = new ArrayList<>(); // Your data here
-        WorryCardAdapter adapter = new WorryCardAdapter(data);
-        recyclerView.setAdapter(adapter);
-
-        return view;
+        return inflater.inflate(R.layout.fragment_ongoing_worries, container, false);
     }
 
     @Override
@@ -42,5 +35,24 @@ public class OngoingWorriesFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         ((MainActivity) requireActivity()).setStatusBarColor(R.color.darkPurpleMain);
 
+        //Set up recycler view
+        List<WorryItem> data = new ArrayList<>();
+        data.add(new WorryItem("TEST", R.drawable.worry_1));
+        data.add(new WorryItem("TEST2", R.drawable.worry_1));
+        data.add(new WorryItem("TEST3", R.drawable.worry_1));
+        data.add(new WorryItem("TEST4", R.drawable.worry_1));
+        data.add(new WorryItem("TEST5", R.drawable.worry_1));
+        data.add(new WorryItem("TEST6", R.drawable.worry_1));
+        data.add(new WorryItem("TEST7", R.drawable.worry_1));
+        data.add(new WorryItem("TEST8", R.drawable.worry_1));
+        data.add(new WorryItem("TEST9", R.drawable.worry_1));
+        data.add(new WorryItem("TEST10", R.drawable.worry_1));
+
+        WorryCardAdapter adapter = new WorryCardAdapter(data);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        recyclerView.setAdapter(adapter);
     }
 }
