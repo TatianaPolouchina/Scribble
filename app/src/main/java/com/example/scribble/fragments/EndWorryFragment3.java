e0285633236f0649c928875ad5cfe99144065029
Tatiana <tpolouch@student.ubc.ca>
2024-07-22 15:26:27 -0400

    Added finish worry animation and replaced background scribble with a new design

--- app/src/main/java/com/example/scribble/fragments/EndWorryFragment3.java
+++ app/src/main/java/com/example/scribble/fragments/EndWorryFragment3.java
@@ -1,5 +1,8 @@
 package com.example.scribble.fragments;
 
+import android.animation.ObjectAnimator;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.VideoView;
 
 import com.example.scribble.R;
 import com.example.scribble.SharedViewModel;
@@ -20,6 +24,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class EndWorryFragment3 extends Fragment {
     private SharedViewModel sharedViewModel;
     private Worry worry;
+    private ImageView ongoingWorryImage;
+    private ImageView finishedWorryImage;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,20 +43,56 @@ public class EndWorryFragment3 extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        ongoingWorryImage = view.findViewById(R.id.worryCharacter);
+        finishedWorryImage = view.findViewById(R.id.finishedWorryCharacter);
 
         //update image
         if (getArguments() != null) {
             worry = (Worry) getArguments().getSerializable("selectedWorry");
             if (worry != null) {
-                ImageView image = view.findViewById(R.id.worryCharacter);
-                image.setImageResource(worry.getFinishedImageResId());
+                ongoingWorryImage.setImageResource(worry.getOngoingImageResId());
+                finishedWorryImage.setImageResource(worry.getFinishedImageResId());
             }
         }
+
+        fadeToFinishedWorry(view);
 
         view.findViewById(R.id.closeButton).setOnClickListener(v -> {
             sharedViewModel.finishWorry(worry);
             BottomNavigationView bottomNav = requireActivity().findViewById(R.id.bottom_navigation);
             bottomNav.setSelectedItemId(R.id.navigation_finished_worries);
         });
+    }
+
+    /***
+     * Plays the animated transition from ongoing to finished worry
+     *
+     * @param view The view containing the worry images
+     */
+    public void fadeToFinishedWorry(View view) {
+        ObjectAnimator fadeOutOngoingWorry = ObjectAnimator.ofFloat(ongoingWorryImage, "alpha", 1f, 0f);
+        ObjectAnimator fadeInFinishedWorry = ObjectAnimator.ofFloat(finishedWorryImage, "alpha", 0f, 1f);
+        fadeOutOngoingWorry.setDuration(1800);
+        fadeInFinishedWorry.setDuration(1500);
+
+        fadeOutOngoingWorry.start();
+        fadeInFinishedWorry.start();
+
+        VideoView videoView = view.findViewById(R.id.animation);
+        Uri videoUri = Uri.parse("android.resource://" + requireContext().getPackageName() + "/" + R.raw.scribble_to_sun_animation);
+        videoView.setVideoURI(videoUri);
+        videoView.setVisibility(View.VISIBLE);
+
+        videoView.setOnPreparedListener(mp -> {
+            videoView.start();
+            mp.setOnInfoListener((mp1, what, extra) -> {
+                if (what == MediaPlayer.MEDIA_INFO_VIDEO_RENDERING_START) {
+                    view.findViewById(R.id.placeholder).setVisibility(View.GONE);
+                    return true;
+                }
+                return false;
+            });
+        });
+
     }
 }
